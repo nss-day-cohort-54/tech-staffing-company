1205d40b91f2e541bf0033c57de4b23e3debba26
Steve Brownlee <chortlehoort@gmail.com>
2022-01-19 09:24:32 -0600

    Initial commit Initial algorithm defined by Devin and Tyler

---
+++ scripts/database.js
@@ -0,0 +1,58 @@
+const database = {
+    candidates: [
+        { "id": 1, "first_name": "Trenna", "last_name": "Satterlee", "email": "tsatterlee0@cdbaby.com", "skill": "Servers" },
+        { "id": 2, "first_name": "Morgen", "last_name": "Eilhersen", "email": "meilhersen1@privacy.gov.au", "skill": "MQL4" },
+        { "id": 3, "first_name": "Casi", "last_name": "McGeechan", "email": "cmcgeechan2@shutterfly.com", "skill": "Thermal Oxidation" },
+        { "id": 4, "first_name": "Trey", "last_name": "Frascone", "email": "tfrascone3@biblegateway.com", "skill": "CBR" },
+        { "id": 5, "first_name": "Barbi", "last_name": "Tayt", "email": "btayt4@51.la", "skill": "cGMP manufacturing" },
+        { "id": 6, "first_name": "Loella", "last_name": "Ciccarello", "email": "lciccarello5@irs.gov", "skill": "EoMPLS" },
+        { "id": 7, "first_name": "Lynnet", "last_name": "Jirus", "email": "ljirus6@sina.com.cn", "skill": "ZBrush" },
+        { "id": 8, "first_name": "Inger", "last_name": "Rehn", "email": "irehn7@blinklist.com", "skill": "Test Automation" },
+        { "id": 9, "first_name": "Bryn", "last_name": "MacKereth", "email": "bmackereth8@cargocollective.com", "skill": "VDSL" },
+        { "id": 10, "first_name": "Eldredge", "last_name": "Venning", "email": "evenning9@mtv.com", "skill": "Fertilizers" },
+        { "id": 11, "first_name": "Blondy", "last_name": "Harrold", "email": "bharrolda@example.com", "skill": "Sh" },
+        { "id": 12, "first_name": "Ida", "last_name": "Zammett", "email": "izammettb@parallels.com", "skill": "DPM" },
+        { "id": 13, "first_name": "Pierrette", "last_name": "Doche", "email": "pdochec@disqus.com", "skill": "Cerner" },
+        { "id": 14, "first_name": "Ingrid", "last_name": "Shearmer", "email": "ishearmerd@weebly.com", "skill": "Hotels" },
+        { "id": 15, "first_name": "Igor", "last_name": "Wheelhouse", "email": "iwheelhousee@vkontakte.ru", "skill": "Administrative Law" },
+        { "id": 16, "first_name": "Tracey", "last_name": "Dryburgh", "email": "tdryburghf@examiner.com", "skill": "vCloud" },
+        { "id": 17, "first_name": "Lisette", "last_name": "Crackel", "email": "lcrackelg@youtube.com", "skill": "Test Automation" },
+        { "id": 18, "first_name": "Erma", "last_name": "Glastonbury", "email": "eglastonburyh@arstechnica.com", "skill": "PWM" },
+        { "id": 19, "first_name": "Gasper", "last_name": "Milligan", "email": "gmilligani@moonfruit.com", "skill": "DFU" },
+        { "id": 20, "first_name": "Thatch", "last_name": "Luney", "email": "tluneyj@sitemeter.com", "skill": "Luxury Lifestyle" },
+        { "id": 21, "first_name": "Nevin", "last_name": "Virgoe", "email": "nvirgoek@seesaa.net", "skill": "Yoga" },
+        { "id": 22, "first_name": "Pepi", "last_name": "Doog", "email": "pdoogl@cargocollective.com", "skill": "ZBrush" },
+        { "id": 23, "first_name": "Reube", "last_name": "Boule", "email": "rboulem@hp.com", "skill": "FBCB2" },
+        { "id": 24, "first_name": "Napoleon", "last_name": "Goreisr", "email": "ngoreisrn@odnoklassniki.ru", "skill": "RPM" },
+        { "id": 25, "first_name": "Dalila", "last_name": "Parkhouse", "email": "dparkhouseo@imgur.com", "skill": "CPI" },
+        { "id": 26, "first_name": "Elsi", "last_name": "Lempenny", "email": "elempennyp@chronoengine.com", "skill": "MD&amp;A" },
+        { "id": 27, "first_name": "Siegfried", "last_name": "Lunn", "email": "slunnq@indiatimes.com", "skill": "Contact Centers" },
+        { "id": 28, "first_name": "Galen", "last_name": "Lackington", "email": "glackingtonr@geocities.com", "skill": "Servers" },
+        { "id": 29, "first_name": "Nicolais", "last_name": "Figurski", "email": "nfigurskis@dyndns.org", "skill": "RNA" },
+        {
+            "id": 30,
+            "first_name": "Cornell",
+            "last_name": "Gocher",
+            "email": "cgochert@tripod.com",
+            "skill": "Servers"
+        }
+    ],
+    companies: [
+        {
+            "id": 1,
+            "company": "DabZ",
+            "address": "675 Namekagon Lane",
+            "skillNeeded": "Test Automation",
+            "engineersNeeded": 15
+        },
+        { "id": 2, "company": "Skyvu", "address": "3 Laurel Park", "skillNeeded": "MCMS", "engineersNeeded": 3 },
+        { "id": 3, "company": "Topicshots", "address": "00847 Harper Circle", "skillNeeded": "XMLBeans", "engineersNeeded": 5 },
+        { "id": 4, "company": "Eimbee", "address": "0612 Washington Circle", "skillNeeded": "MSP Practitioner", "engineersNeeded": 12 },
+        { "id": 5, "company": "Jabbersphere", "address": "13609 Londonderry Drive", "skillNeeded": "Charitable Giving", "engineersNeeded": 12 },
+        { "id": 6, "company": "BlogXS", "address": "186 Almo Drive", "skillNeeded": "Payroll", "engineersNeeded": 9 },
+        { "id": 7, "company": "Mynte", "address": "573 Mayfield Point", "skillNeeded": "ZBrush", "engineersNeeded": 2 },
+        { "id": 8, "company": "Browsetype", "address": "42532 Jenifer Terrace", "skillNeeded": "Twist", "engineersNeeded": 16 },
+        { "id": 9, "company": "Kare", "address": "3 Dayton Road", "skillNeeded": "QS9000", "engineersNeeded": 11 },
+        { "id": 10, "company": "Chatterbridge", "address": "74 Blackbird Drive", "skillNeeded": "Servers", "engineersNeeded": 14 }
+    ]
+}
---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+/*
+    1. See if any candidates have the skills that each employer needs
+    2. If there are, create a new data structure that stores the primary
+        key of the employer, and an array of matching candidates
+    3. Display each company name, the name of the matching candidates
+        a message if the company can fill their needs
+*/
+
+// Get the data
+
+
+// Initialize an array of company candidates
+
+
+// Iterate the array of company objects
+
+    // Create an object that stores company id and empty
+    // array for candidates
+
+    /*
+        {
+            companyId: {number},
+            matchingCandidates: {array}
+        }
+    */
+
+
+    // Within above loop, iterate candidate objects array
+
+        // If statement checking to see if company skill needed
+        // equals the candidate skill
+
+            // If true, add to array of matches created above
+
+
+// Iterate company object array
+    // Iterate company candidates array
+        // If companyId matches company id
+            // If true, print company name
+            // Iterate array of matching candidates
+            /*
+                Check if length of matching candidates array is
+                greater than or equal to the engineersNeeded
+                property on the company object
+
+                If true, display "can staff" message
+                Else display "cannot staff" message
+            */
